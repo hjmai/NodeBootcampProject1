637df0907018e7e372052d2b1ed8712d357bd6ac
Jonathan Mai <h.jmai2791@gmail.com>
2018-06-11 19:01:43 -0700

    image correction

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -249,7 +249,7 @@ database.ref('decks/').on('value', function (snapshot) {
         var deckClass = obj.selectedDeck.deckClass.toLowerCase();
         var button = $('<button class="btn purple deckBtn waves-effect">');
         button.data("key", obj.selectedDeck);
-        button.html('<img class="classIcon" style="height: 30px; width: 30px;" src="./assets/images/' + deckClass.toLowerCase() + '.png"> ' + obj.selectedDeck.name);
+        button.html('<img class="classIcon" style="height: 30px; width: 30px;" src="/images/' + deckClass.toLowerCase() + '.png"> ' + obj.selectedDeck.name);
         button.prepend("&uarr; " + (obj.selectedDeck.upvotes - obj.selectedDeck.downvotes) + " ");
         // button.text(obj.selectedDeck.name);
         $('.deckList').append(button);
